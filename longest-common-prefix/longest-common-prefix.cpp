3166e142a643f47f9b7b80973d21064e2843818c
Megha <meghagupta19082001@gmail.com>
2021-12-23 18:07:41 +0530

    Time: 0 ms (100.00%), Space: 9 MB (99.64%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s="";
+        
+        sort(strs.begin(),strs.end());
+        
+        for(int i=0;i<strs[0].size();i++)
+        {
+            if(strs[0][i]==strs[strs.size()-1][i])
+              s+=strs[0][i]; 
+            else break;
+        }
+        return s;
+    }
+};
