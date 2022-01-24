354d3f0ccda953a5a72f231945de2a09b4bcbfdb
Megha <meghagupta19082001@gmail.com>
2022-01-24 12:53:52 +0530

    Time: 20 ms (14.33%), Space: 5.9 MB (24.56%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0||x==1)
+            return x;
+        long long i;
+        for(i=1;i*i<=x;i++);
+        return i-1;
+    }
+};
